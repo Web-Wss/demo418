87da7870487ca9e302e40efdc3ee637f00d35f03
Web-Wss <1306822660@qq.com>
2024-04-19 14:51:12 +0800

    添加了防抖

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,42 +1,29 @@
 <template>
 	<view class="content">
 		<!-- demo -->
-		<scroll-view 
-		class="top" 
-		scroll-x="true" 
-		:scroll-left="left"
-		 @scroll="handleScrollTop">
+		<scroll-view class="top" scroll-x="true" :scroll-left="left" @scroll="handleScrollTop">
 			<view class="list" v-for="i in 10" :key="i">
 				薄膜#{{i}}
 			</view>
 		</scroll-view>
-		<scroll-view  
-		scroll-y="true" 
-		class="left"  
-		@scroll="handleScrollLeft"
-		:scroll-top="top">
+		<scroll-view scroll-y="true" class="left" @scroll="handleScrollLeft" :scroll-top="top">
 			<view class="list" v-for="i in 10" :key="i">
 				属性{{i}}
 			</view>
 		</scroll-view>
 		<view class="content">
-			<scroll-view 
-			class="scroll" 
-			scroll-y="true"   
-			scroll-x="true" 
-			:scroll-left="left"
-			:scroll-top="top"
-			@scroll="handleScrollContent">
+			<scroll-view class="scroll" scroll-y="true" scroll-x="true" :scroll-left="left" :scroll-top="top"
+				@scroll="handleScrollContent">
 				<table border="1">
 					<tbody>
 						<tr v-for="i in 10" :key="i">
 							<td v-for="j in 10" :key="j">
 								<view class="span">
 									<view class="btn" @click="btn(i,j,'合格')">
-										合格
+										{{j}}-{{i}}合格
 									</view>
 									<view class="btn" @click="btn(i,j,'不合格')">
-										不合格
+										{{j}}-{{i}}不合格
 									</view>
 								</view>
 							</td>
@@ -50,28 +37,41 @@
 </template>
 
 <script>
+	import debounce from '../../utils/utils';
 	export default {
 		data() {
 			return {
-				top:0,
-				left:0
+				top: 0,
+				left: 0
 			}
 		},
-		onLoad() {
-		},
-	
+		onLoad() {},
+
 		methods: {
-			handleScrollContent(event) {
+			// handleScrollContent(event) {
+			// 	this.top = event.detail.scrollTop;
+			// 	this.left = event.detail.scrollLeft;
+			// },
+			// handleScrollTop(event){
+			// 	this.left = event.detail.scrollLeft;
+			// },
+			// handleScrollLeft(event){
+			// 	this.top = event.detail.scrollTop;
+			// },
+
+			handleScrollContent: debounce(function(event) {
 				this.top = event.detail.scrollTop;
 				this.left = event.detail.scrollLeft;
-			},
-			handleScrollTop(event){
-				this.left = event.detail.scrollLeft;
-			},
-			handleScrollLeft(event){
-				this.top = event.detail.scrollTop;
-			},
-			btn(i,j,info){
+			}, 100),
+			handleScrollTop: debounce(function(event) {
+					this.left = event.detail.scrollLeft;
+			}, 100),
+			handleScrollLeft: debounce(function(event) {
+					this.top = event.detail.scrollTop;
+			}, 100),
+
+
+			btn(i, j, info) {
 				console.log(`您点击了薄膜${j},属性${i},${info}`);
 			}
 		}
